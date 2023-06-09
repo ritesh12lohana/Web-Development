f1caee3dc19a47e6ad9c200957b7fc89878fc26a
ritesh12lohana <97667859+ritesh12lohana@users.noreply.github.com>
2023-06-09 13:24:47 +0500

    Create menuApi.js

---
+++ Restaurant/src/menuApi.js
@@ -0,0 +1,93 @@
+const Menu = [
+  {
+    id: 1,
+    image:
+      "https://geekrobocook.com/wp-content/uploads/2021/03/46_Mexican-Maggi.jpg",
+    name: "maggi",
+    category: "breakfast",
+    price: "12₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . "
+  },
+
+  {
+    id: 2,
+    image: "https://i.ytimg.com/vi/R64zXJFvGSU/maxresdefault.jpg",
+    name: "allu pakoida",
+    category: "evening",
+    price: "20₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . "
+  },
+  {
+    id: 3,
+    image:
+      "https://www.verywellfit.com/thmb/dEMxM4XSJRZGW8LTAUvAjzx2ZZw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/P_20170813_183320_1_1-59a392bfaad52b001137445b.jpg",
+    name: "corn",
+    category: "breakfast",
+    price: "10₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . "
+  },
+  {
+    id: 4,
+    image: "https://spicetrekkers.com//upload/recettes/pois-chiches-sindhi.jpg",
+    name: "chola",
+    category: "lunch",
+    price: "50₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . "
+  },
+  {
+    id: 5,
+    image:
+      "https://www.simplyrecipes.com/thmb/KRw_r32s4gQeOX-d07NWY1OlOFk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1c-c2b1885d27d4481c9cfe6f6286a64342.jpg",
+    name: "pizza",
+    category: "evening",
+    price: "80₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . "
+  },
+  {
+    id: 6,
+    image:
+      "https://www.foodandwine.com/thmb/Wd4lBRZz3X_8qBr69UOu2m7I2iw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/classic-cheese-pizza-FT-RECIPE0422-31a2c938fc2546c9a07b7011658cfd05.jpg",
+    name: "Non-Veg Thali",
+    category: "dinner",
+    price: "180₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . "
+  },
+  {
+    id: 7,
+    image:
+      "https://propakistani.pk/foodnama/wp-content/uploads/2022/01/group-indian-assorted-sweets-mit.jpg",
+    name: "Sweets",
+    category: "dinner",
+    price: "60₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . "
+  },
+  {
+    id: 8,
+    image:
+      "https://kfoods.com/images1/newrecipeicon/crispy-zinger-burger_9996.jpg",
+    name: "Zinger Burger",
+    category: "lunch",
+    price: "60₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . "
+  },
+  {
+    id: 9,
+    image:
+      "https://holycowvegan.net/wp-content/uploads/2022/06/samosa-recipe.jpg",
+    name: "samaso",
+    category: "evening",
+    price: "10₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . "
+  }
+];
+
+export default Menu;
